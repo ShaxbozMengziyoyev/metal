54f56eda735426836cea5140c9bd1f50b6990bfb
ShaxbozMengziyoyev <shaxboz370@gmail.com>
2022-11-05 12:44:46 +0500

    commit

--- src/util/postgres.js
+++ src/util/postgres.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    connectionString: 'postgres://postgres:1324306@localhost:5432/sms',
-    elephantConnectionString: 'postgres://okswmsar:Qr0IWhS2oCGlSqbFXlhTEbpQsS6Gumrf@heffalump.db.elephantsql.com/okswmsar'
+    connectionString: 'postgres://okswmsar:Qr0IWhS2oCGlSqbFXlhTEbpQsS6Gumrf@heffalump.db.elephantsql.com/okswmsar'
+    // elephantConnectionString: 'postgres://okswmsar:Qr0IWhS2oCGlSqbFXlhTEbpQsS6Gumrf@heffalump.db.elephantsql.com/okswmsar'
 })
 
 class PG {
